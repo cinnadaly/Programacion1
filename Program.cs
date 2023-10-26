9d180b380b1a201d4a43743256d84f14f9faf55a
cinnadaly <149030309+cinnadaly@users.noreply.github.com>
2023-10-25 20:31:38 -0700

    Add files via upload
    Código sumatoria de números para cuando se detecta un 0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num, sumatoria = 0;
+
+        Console.WriteLine("Ingrese un numero");
+        num = int.Parse(Console.ReadLine());
+
+        while (num > 0)
+        {
+            sumatoria = sumatoria + num;
+            Console.WriteLine("Ingrese otro numero");
+            num = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(sumatoria);
+    }
+}
