a4d6a007c3ff67a41e382a97e7130ce596e70da0
cinnadaly <149030309+cinnadaly@users.noreply.github.com>
2023-11-01 19:18:40 -0700

    ATM
    Código que da cambio en billetes y muestra el cambio

---
+++ codigo atm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class ATM
+{
+    public static void Main(string[] args)
+    {
+        int Cantdin;
+        do
+        {
+            Console.WriteLine("Que cantidad de dinero desea cambiar?");
+            Cantdin = int.Parse(Console.ReadLine());
+
+            Arreglobilletes(Cantdin);
+            Console.WriteLine("Desea volver a consultar a cambiar dinero? (S/N) ");
+        }
+        while (Console.ReadLine().ToLower() == "s");
+    }
+    
+        static void Arreglobilletes(int Cantdin)
+        {
+            int[] billetes = { 1000, 500, 200, 100, 50, 20 };
+        Console.WriteLine("Tu cambio es: ");
+        
+        
+        foreach (var billete in billetes)
+            {
+            int sobrante;
+            int cambio = Cantdin / billete;
+            if(cambio > 0) 
+            {
+                Console.WriteLine($"{cambio} billetes de {billete} ");
+                Cantdin%=billete;
+               
+            }
+            }
+        if (Cantdin > 0)
+        {
+            Console.WriteLine($"El sobrante es {Cantdin} pesos");
+        }
+       }
+    }
