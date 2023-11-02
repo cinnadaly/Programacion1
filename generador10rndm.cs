8a6a324fb29129721aee565b03c883e16f1ab0d7
cinnadaly <149030309+cinnadaly@users.noreply.github.com>
2023-11-01 20:42:47 -0700

    Add files via upload
    Generador de 10 números random(1-100) dentro de un ciclo

---
+++ generador10rndm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class generador
+{
+    public static void Main(string[] args)
+    {
+        do
+        {
+            Numrandoms();
+            Console.WriteLine("Desea volver a generar 10 numeros aleatorios? (S/N)");
+        }
+        while (Console.ReadLine().ToLower() == "s");
+      
+
+        static void Numrandoms()
+        {
+            int[] nums = new int[10];
+            Random aleatorio = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = aleatorio.Next(0, 100);
+            }
+            foreach (var num in nums)
+            {
+                Console.WriteLine($"Numero: {num}");
+            }
+        }
+    }
+}
