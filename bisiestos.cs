032ac95887278fa81715e287fdae07b19898a47c
cinnadaly <149030309+cinnadaly@users.noreply.github.com>
2023-11-01 20:29:26 -0700

    A BIS
    Código que verifica si un ano es bisiesto o no

---
+++ bisiestos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Clase9
+{
+    public static void Main(string[] args)
+    {
+        int Ano;
+        do
+        {
+            Console.WriteLine("Ingrese el año que desea consultar");
+            Ano = int.Parse(Console.ReadLine());
+
+            Arregloano(Ano);
+            Console.WriteLine("Desea volver a consultar si un año es bisiesto? (S/N) ");
+        }
+        while (Console.ReadLine().ToLower() == "s");
+
+        static void Arregloano(int Ano)
+        {
+            if (Ano % 4 != 0)
+            { 
+                Console.WriteLine("No es bisiesto"); 
+            }
+
+            if (Ano % 4 == 0 && Ano % 100 != 0) 
+            { 
+                Console.WriteLine("Es bisisesto");
+            }
+
+            if (Ano % 4 == 0 && Ano % 100 == 0 && Ano % 400 == 0)
+            { 
+                Console.WriteLine("Es bisiesto"); 
+            }
+
+            else if (Ano % 4 == 0 && Ano % 100 == 0 && Ano % 400 != 0)
+            {
+                Console.WriteLine("No es bisiesto");
+            }
+         }
+     }
+}
+    
