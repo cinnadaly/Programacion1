da9313e6a6883e5513c4524742aeb1226e10bd13
cinnadaly <149030309+cinnadaly@users.noreply.github.com>
2023-11-08 20:12:06 -0800

    Cap de 5 pasos
    Programa que captura 10 números, los imprime, realiza e imprime la sumatoria, ordena los elementos de menor a mayor y busca un numero a base de la posición dada por el usuario.

---
+++ capturador con Funciones.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+public class Capturar10num
+{
+    public static void Main(string[] args)
+    {
+        int[] numeros = new int[10];
+
+        Capturador(numeros);
+
+        Console.WriteLine("Los números ingresados son:");
+        Imprimnum(numeros);
+
+        int sumatoria = Sumatoriatotal(numeros); 
+        Console.WriteLine($"La suma de los números ingresados es: {sumatoria}");
+
+        int[] ascendente = MenorAMayor(numeros);
+        Console.WriteLine("Los números de menor a mayor son:");
+        Imprimnum(ascendente);
+
+        BuscarPorPosicion(numeros);
+
+    }
+
+    static void Capturador(int[] numeros)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Ingrese un número entero:");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    static void Imprimnum(int[] numeros)
+    {
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    static int Sumatoriatotal(int[] numeros)
+    {
+        int sumatoria = 0;
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            sumatoria += numeros[i];
+        }
+        return sumatoria;
+    }
+
+    static int[] MenorAMayor(int[] numeros)
+    {
+        int[] ascendente = new int[numeros.Length];
+        Array.Copy(numeros, ascendente, numeros.Length);
+
+        Array.Sort(ascendente);
+
+        return ascendente;
+    }
+
+    static void BuscarPorPosicion(int[] numeros)
+    {
+        int numpos = 0;
+        Console.WriteLine("Ingrese la posicion que desea consultar: ");
+        numpos = Convert.ToInt32(Console.ReadLine());
+
+        if (numpos >=1 && numpos <= numeros.Length)
+        {
+            int numbuscado = numeros[numpos - 1];
+            Console.WriteLine($"{numbuscado} se encuentra en la posicion {numpos} de la lista");
+        }
+        else
+        {
+            Console.WriteLine($"{numpos} no se encuentra en la lista");
+        }
+    }
+}
+
+
