455e77fb949716e08a89c31affc1ab0b70c9fa9d
cinnadaly <149030309+cinnadaly@users.noreply.github.com>
2023-11-22 19:26:52 -0800

    Contar vs
    Código que cuenta cuantas vocales tiene una palabra

---
+++ ContadorDeVocales.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class ContadorVocales
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa una palabra");
+        string palabra = Console.ReadLine();
+
+        ContarVocales(palabra);
+    }
+
+    static void ContarVocales(string palabra)
+    {
+        int a = 0;
+        int e = 0;
+        int i = 0;
+        int o = 0;
+        int u = 0;
+
+        foreach (char letra in palabra)
+        {
+            if (letra == 'a' || letra == 'A')
+            {
+                a++;
+            }
+            else if (letra == 'e' || letra == 'E')
+            {
+                e++;
+            }
+            else if (letra == 'i' || letra == 'I')
+            {
+                i++;
+            }
+            else if (letra == 'o' || letra == 'O')
+            {
+                o++;
+            }
+            else if (letra == 'u' || letra == 'U')
+            {
+                u++;
+            }
+        }
+
+        Console.WriteLine($"La palabra '{palabra}' tiene:");
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"e = {e}");
+        Console.WriteLine($"i = {i}");
+        Console.WriteLine($"o = {o}");
+        Console.WriteLine($"u = {u}");
+    }
+}
